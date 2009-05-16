688281c831fd0589512b9c39f785de9af7ac6238
christopher.rabotin <christopher.rabotin@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-05-16 01:50:42 +0000

    Ce code fonctionne avec Arduino. J'ai actuellement toujours le même problème de compilation que précédemment. On m'a indiqué que c'est parce que je compilais pour le mauvais microcontrolleur (un atmega16 aulieu d'un atmega168). Malheureusement, je n'arrive pas à changer ces valeurs. Ainsi je code sous Arduino pour le moment...

--- trunk/Elec/src-test/atmega168-test.c
+++ trunk/Elec/src-test/atmega168-test.c
@@ -11,16 +11,36 @@
  * http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
  */
 int ledPin = 13; // LED connected to digital pin 13
+int value = LOW; // previous value of the LED
+long previousMillis = 0; // will store last time LED was updated
 
 void setup() // run once, when the sketch starts
 {
 	pinMode(ledPin, OUTPUT); // sets the digital pin as output
 }
 
-void loop() // run over and over again
-{
-	digitalWrite(ledPin, HIGH); // sets the LED on
-	delay(1000); // waits for a second
-	digitalWrite(ledPin, LOW); // sets the LED off
-	delay(1000); // waits for a second
+void loop() {
+	int add=500, i=0;
+	long interval = 5000; // interval at which to blink (milliseconds)
+	// here is where you'd put code that needs to be running all the time.
+
+	// check to see if it's time to blink the LED; that is, is the difference
+	// between the current time and last time we blinked the LED bigger than
+	// the interval at which we want to blink the LED.
+	for (i = 0; i < 10; i++) {
+
+		if (millis() - previousMillis > interval) {
+			previousMillis = millis(); // remember the last time we blinked the LED
+
+			// if the LED is off turn it on and vice-versa.
+			if (value == LOW)
+				value = HIGH;
+			else
+				value = LOW;
+
+			digitalWrite(ledPin, value);
+		}
+		interval = interval - add;
+		delay(add);
+	}
 }
