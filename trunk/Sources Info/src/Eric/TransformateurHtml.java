34f97030bd2dee2456414503a496434904a9eb68
ehau78 <ehau78@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-09 09:31:31 +0000

    Création d'un convertiseur html/java

---
+++ trunk/Sources Info/src/Eric/TransformateurHtml.java
@@ -0,0 +1,39 @@
+package Eric;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+	
+
+public class TransformateurHtml {
+	private static String fichier_Html = "fichier_html.txt";
+	private static String fichier_sortie = "fichier_java.txt";
+	public static void main(String[] args) {
+		try {
+			InputStream ips = new FileInputStream(fichier_Html);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			OutputStream ops = new FileOutputStream(fichier_sortie);
+			OutputStreamWriter opsw = new OutputStreamWriter(ops);
+			BufferedWriter  bw = new BufferedWriter(opsw);
+			String ligne;
+			ligne = br.readLine();
+			while(ligne !=null){
+				ligne=ligne.replace("\"", "\\\"");
+				System.out.println(ligne);
+				ligne = "out.println(\""+ligne+"\");\n";
+				bw.write(ligne);
+				ligne = br.readLine();
+			}
+			bw.close();
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+}
