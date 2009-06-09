bb2e4da5011e12c3eb2ee8ddf533bc1b45e3d0b8
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-09 15:17:05 +0000

    cleanup et statiquisation

--- Sources Info/src/Raphaël/FFT.java
+++ Sources Info/src/Raphaël/FFT.java
@@ -1,151 +1,32 @@
-import javasci.*;
-
 /*  
  *  source : http://www.ling.upenn.edu/~tklee/Projects/dsp/
  */
 
 public class FFT
-{    
-//    int n, m;
-//    // Lookup tables.  Only need to recompute when size of FFT changes.
-//    double[] cos;
-//    double[] sin;
-//    double[] window;
-//
-//    public FFT(int n)
-//    {
-//        this.n = n;
-//
-//        // Make sure n is a power of 2
-//        if (n != (1 << lg(n)))
-//        {
-//            throw new RuntimeException("FFT length must be power of 2");
-//        }
-//
-//        // precompute tables
-//        cos = new double[n / 2];
-//        sin = new double[n / 2];
-//
-////     for(int i=0; i<n/4; i++) {
-////       cos[i] = Math.cos(-2*Math.PI*i/n);
-////       sin[n/4-i] = cos[i];
-////       cos[n/2-i] = -cos[i];
-////       sin[n/4+i] = cos[i];
-////       cos[n/2+i] = -cos[i];
-////       sin[n*3/4-i] = -cos[i];
-////       cos[n-i]   = cos[i];
-////       sin[n*3/4+i] = -cos[i];        
-////     }
-//
-//        for (int i = 0; i < n / 2; i++)
-//        {
-//            cos[i] = Math.cos(-2 * Math.PI * i / n);
-//            sin[i] = Math.sin(-2 * Math.PI * i / n);
-//        }
-//
-//        makeWindow();
-//    }
-//
-//    protected void makeWindow()
-//    {
-//        // Make a Blackman window:
-//        // w(n)=0.42-0.5cos{(2*PI*n)/(N-1)}+0.08cos{(4*PI*n)/(N-1)};
-//        window = new double[n];
-//        for (int i = 0; i < window.length; i++)
-//        {
-//            window[i] = 0.42 - 0.5 * Math.cos(2 * Math.PI * i / (n - 1)) + 0.08 * Math.cos(4 * Math.PI * i / (n - 1));
-//        }
-//    }
-//
-//    /*
-//     * http://cnx.rice.edu/content/m12016/latest/
-//     */
-//public void fft(double[] x)
-//    {
-//        int i, j, k, n1, n2, a;
-//        double c, s, e, t1, t2;
-//        double[] y = new double[x.length];
-//
-//
-//        // Bit-reverse
-//        j = 0;
-//        n2 = n / 2;
-//        for (i = 1; i < n - 1; i++)
-//        {
-//            n1 = n2;
-//            while (j >= n1)
-//            {
-//                j = j - n1;
-//                n1 = n1 / 2;
-//            }
-//            j = j + n1;
-//
-//            if (i < j)
-//            {
-//                t1 = x[i];
-//                x[i] = x[j];
-//                x[j] = t1;
-//                t1 = y[i];
-//                y[i] = y[j];
-//                y[j] = t1;
-//            }
-//        }
-//
-//        // FFT
-//        n1 = 0;
-//        n2 = 1;
-//
-//        for (i = 0; i < m; i++)
-//        {
-//            n1 = n2;
-//            n2 = n2 + n2;
-//            a = 0;
-//
-//            for (j = 0; j < n1; j++)
-//            {
-//                c = cos[a];
-//                s = sin[a];
-//                a += 1 << (m - i - 1);
-//
-//                for (k = j; k < n; k = k + n2)
-//                {
-//                    t1 = c * x[k + n1] - s * y[k + n1];
-//                    t2 = s * x[k + n1] + c * y[k + n1];
-//                    x[k + n1] = x[k] - t1;
-//                    y[k + n1] = y[k] - t2;
-//                    x[k] = x[k] + t1;
-//                    y[k] = y[k] + t2;
-//                }
-//            }
-//        }
-//
-//        for (i = 0; i < x.length; i++)
-//        {
-//            x[i] = Math.sqrt(x[i] * x[i] + y[i] * y[i]);
-//        }
-//    }
-//    
+{
     // Test the FFT to make sure it's working
     public static void main(String[] args)
     {
         Capture capture = new Capture();
         int[] audio = capture.ecoute(1000);
+                
+        double[] audioDouble = FFT.zeroPadding(audio);
         
-        //conversion en double
-        double[] audioDouble = new double[audio.length];
-        for(int i = 0; i < audio.length; i++)
-            audioDouble[i] = (double)audio[i];
-        
-        audioDouble = FFT.zeroPadding(audioDouble);
-        
-        FFT fft = new FFT();
-        audioDouble = fft.fftMag(audioDouble);
+        audioDouble = FFT.fftMag(audioDouble);
         
         for(int i = 0; i < audioDouble.length; i++)
             System.out.println(audioDouble[i]);
     }
     
-    private static double[] zeroPadding(double[] sig)
+    /**
+     * Complete le signal sig avec des zeros 
+     * jusqu'à atteindre une longeur qui soit une puissance de 2
+     * et le transforme en double au passage
+     * 
+     * @param sig signal originel
+     * @return signal de longueur une puissance entière de 2
+     */
+    private static double[] zeroPadding(int[] sig)
     {
         double[] padded = new double[1 << (lg(sig.length) + 1)];
         for(int i = 0; i < sig.length; i++)
@@ -154,9 +35,8 @@ public class FFT
         return padded;
     }
     
-    private int nu, n;
-
-    private int bitrev(int j)
+    //TODO : comprendre & commenter
+    private static int bitrev(int j, int nu)
     {
         int j2;
         int j1 = j;
@@ -170,11 +50,12 @@ public class FFT
         return k;
     }
 
-    public double[] fftMag(double[] x)
+    //TODO : comprendre & commenter
+    public static double[] fftMag(double[] x)
     {
         // assume n is a power of 2
-        n = x.length;
-        nu = lg(n);
+        int n = x.length;
+        int nu = lg(n);
         int n2 = n / 2;
         int nu1 = nu - 1;
         double[] xre = new double[n];
@@ -194,7 +75,7 @@ public class FFT
             {
                 for (int i = 1; i <= n2; i++)
                 {
-                    p = bitrev(k >> nu1);
+                    p = bitrev(k >> nu1, nu);
                     arg = 2 * (double) Math.PI * p / n;
                     c = (double) Math.cos(arg);
                     s = (double) Math.sin(arg);
@@ -216,7 +97,7 @@ public class FFT
         int r;
         while (k < n)
         {
-            r = bitrev(k);
+            r = bitrev(k, nu);
             if (r > k)
             {
                 tr = xre[k];
