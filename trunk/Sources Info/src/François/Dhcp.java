19f45cad59b8f07147af9b456ade579469f3d181
cnous3@gmail.com <cnous3@gmail.com@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-10 09:41:32 +0000

    Amélioration de la recherche de la mac, début de la procédure d'ajout au dhcpd.conf

--- trunk/Sources Info/src/François/Dhcp.java
+++ trunk/Sources Info/src/François/Dhcp.java
@@ -11,28 +11,31 @@ public class Dhcp {
 	public static void rechercheAdresseXport(){
 		Shell sh = new Shell(); 
 		GregorianCalendar calendar = new GregorianCalendar();
-		String result = "pas de retour";
+		String resultLog = "pas de retour";
 		try {
-			result = sh.command("tail /var/log/syslog | grep ker").consumeAsString();
-			if(result.equals("s")){
+			resultLog = sh.command("tail /var/log/syslog | grep ker").consumeAsString();
+			if(resultLog.equals("s")){
 				System.out.println(calendar.getTime()+" -> pas de requetes reçues par le DHCP");
 			} else {
-				StringTokenizer st = new StringTokenizer(result);
-				while (st.hasMoreTokens()) {
+				StringTokenizer stLog = new StringTokenizer(resultLog);
+				while (stLog.hasMoreTokens()) {
 					
 					//On regarde si la mac qui tente une requete dhcp est un module xport
 					//si oui on l'ajoute au dhcpd.conf
-					String stCourante = st.nextToken();
+					String stCouranteLog = stLog.nextToken();
 					//taille d'une adresse MAC
-					if(stCourante.length()==17){
-						System.out.println(stCourante);
-						if(stCourante.substring(0, 8).equals("00:20:4A")){
+					if(stCouranteLog.length()==17){
+						System.out.println(stCouranteLog);
+						if(stCouranteLog.substring(0, 8).equals("00:20:4A")){
 							
 						}
 						//on regarde si la MAC existe déjà dans le dhcpd.conf
 						//si oui on ne fait rien, sinon on l'ajoute au fichier 
-						
-						if(stCourante.equals("")){
+						String resultDhcp = sh.command("cat /etc/dhcp3/dhcpd.conf | grep hardware").consumeAsString();
+						StringTokenizer stDhcp = new StringTokenizer(resultDhcp);
+						while (stDhcp.hasMoreTokens()) {
+							String stCouranteDhcp = stDhcp.nextToken();
+						if(stCouranteDhcp.equals("")){
 							
 						}
 					}
@@ -43,12 +46,14 @@ public class Dhcp {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-	      System.out.println(result);
 		
 
 //		DHCPDISCOVER from 00:1c:23:82:e2:f1 (Laptop) via eth0
 
 //		XPORT MAC address: 00:20:4A:xx:xx:xx
+	      
+//	      host XPORT1  {
+//	    	     hardware ethernet 00:06:9a:f3:07:01; fixed-address 192.168.0.10;
+//	    	     }
 	}
 }
