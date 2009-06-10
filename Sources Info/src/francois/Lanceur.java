47a3a9846b9b835220f8134c2661b2f3a7a223fd
cnous3@gmail.com <cnous3@gmail.com@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-10 11:11:29 +0000

    changement du nom de package car MACOS c'est de la merde !!!!!!!

---
+++ Sources Info/src/francois/Lanceur.java
@@ -0,0 +1,14 @@
+package francois;
+
+
+
+public class Lanceur {
+
+	
+	public static void main(String[] args){
+		new Config("/home/cnous3/coding/PR302/Sensitive Home Info/src/François/config.xml");
+		
+		Dhcp.rechercheAdresseXport();
+		
+	}
+}
