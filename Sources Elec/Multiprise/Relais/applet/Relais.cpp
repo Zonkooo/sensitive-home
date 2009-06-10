a2c3e98af47b87cee9ca62a42e68069a5a7289fd
christopher.rabotin <christopher.rabotin@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-10 09:25:25 +0000

    Ajout du test Relais + màj SleepTest (permet maintenant de contrôler aussi en PWM) et début main.pde.

---
+++ Sources Elec/Multiprise/Relais/applet/Relais.cpp
@@ -0,0 +1,35 @@
+#include "WProgram.h"
+void setup();
+void loop();
+int pinCmd = 4;
+short int cmd = true;
+void setup() {
+	Serial.begin(9600); // permet de communiquer en Serial via Arduino IDE
+}
+
+void loop() {
+	delay(50);
+	if (Serial.available()) {
+		int val = Serial.read();
+		if (val == 'O') {
+			Serial.println("Serial: Commande ON");
+			digitalWrite(12, HIGH);
+		} else if (val == 'F') {
+			Serial.println("Serial: Commande OFF");
+			digitalWrite(12, LOW);
+		}
+	}
+}
+
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
---
+++ Sources Elec/Multiprise/main.pde
@@ -0,0 +1,12 @@
+void setup(){
+	
+}
+
+void loop(){
+	/*
+	 * dans le loop on doit d'abord vérifier la connexion au serveur
+	 * ensuite, le serveur doit nous dire si on est connu ou non.
+	 * Si on est connu alors la multiprise nous envoye la liste des modules de capteurs
+	 * que l'on contrôle 
+	 */
+}
