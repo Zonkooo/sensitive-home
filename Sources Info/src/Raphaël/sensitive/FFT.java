210da62d5ffb9c076cba0cad14e25763cfd8ddd8
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-11 15:56:12 +0000

    commit du soir

--- Sources Info/src/Raphaël/sensitive/FFT.java
+++ Sources Info/src/Raphaël/sensitive/FFT.java
@@ -10,41 +10,41 @@ public class FFT
     {
         Capture capture = new Capture();
         int[] audio;
-        double[] audioDouble1, audioDouble2;
+        double[] audioDouble, bouton1, bouton2;
+		
+		System.out.println("tapez le bouton 1");
+		audio = capture.getTap();
+		bouton1 = fftMag(zeroPadding(Outils.normalize(audio)));
+		System.out.println("encore !");
+		audio = capture.getTap();
+		audioDouble = fftMag(zeroPadding(Outils.normalize(audio)));
+		
+		for(int i = 0; i < bouton1.length; i++)
+			bouton1[i] = bouton1[i] + audioDouble[i];
+		
+		System.out.println("tapez le bouton 2");
+		audio = capture.getTap();
+		bouton2 = fftMag(zeroPadding(Outils.normalize(audio)));
+		System.out.println("encore !");
+		audio = capture.getTap();
+		audioDouble = fftMag(zeroPadding(Outils.normalize(audio)));
+		
+		for(int i = 0; i < bouton1.length; i++)
+			bouton2[i] = bouton2[i] + audioDouble[i];
 		
 		while(true)
 		{
 			audio = capture.getTap();
-			System.out.println("1");
-			audioDouble1 = FFT.fftMag(FFT.zeroPadding(normalize(audio)));
+			audioDouble = fftMag(zeroPadding(Outils.subMoy(Outils.normalize(audio))));
+			
+			double c1 = correlation(audioDouble, bouton1);
+			double c2 = correlation(audioDouble, bouton2);
 
-			audio = capture.getTap();
-			System.out.println("2");
-			audioDouble2 = FFT.fftMag(FFT.zeroPadding(normalize(audio)));
-
-			System.out.println(correlation(audioDouble1, audioDouble2));
+			if(c2 > c1)
+				System.out.println("bouton2");
+			else
+				System.out.println("bouton1");
 		}
-    }
-    
-    /**
-     * normalise le vecteur suivant la norme 1
-     * et le convertit en double[]
-     * 
-     * @param vect vecteur à normaliser
-     * @return vecteur normalisé
-     */
-    public static double[] normalize(int[] vect)
-    {
-        double som = 0;
-        double[] norm = new double[vect.length];
-        
-        for (int i = 0; i < vect.length; i++)
-            som += vect[i];
-        
-        for (int i = 0; i < vect.length; i++)
-            norm[i] = vect[i]/som;
-        
-        return norm;
     }
     
     /**
@@ -56,7 +56,7 @@ public class FFT
      */
     public static double[] zeroPadding(double[] sig)
     {
-        double[] padded = new double[1 << ((Outils.lg(sig.length)) + 1)];
+        double[] padded = new double[65536/*1 << ((Outils.lg(sig.length)) + 1)*/];
         for(int i = 0; i < sig.length; i++)
             padded[i] = sig[i];
         
@@ -92,10 +92,10 @@ public class FFT
         for (int i = 0; i < n; i++)
         {
             xre[i] = x[i];
-            xim[i] = 0.0f;
+            xim[i] = 0.0;
         }
-        int k = 0;
-
+		
+		int k = 0;
         for (int l = 1; l <= nu; l++)
         {
             while (k < n)
@@ -137,10 +137,10 @@ public class FFT
             k++;
         }
 
-        mag[0] = (double) (Math.sqrt(xre[0] * xre[0] + xim[0] * xim[0])) / n;
-        for (int i = 1; i < n / 2; i++)
+        //mag[0] = (double) (Math.sqrt(xre[0] * xre[0] + xim[0] * xim[0])) / n;
+        for (int i = 0; i < n / 2; i++)
         {
-            mag[i] = 2 * (double) (Math.sqrt(xre[i] * xre[i] + xim[i] * xim[i])) / n;
+            mag[i] = (double) (Math.sqrt(xre[i] * xre[i] + xim[i] * xim[i]));
         }
         return mag;
     }
@@ -149,7 +149,7 @@ public class FFT
     {
 		double correlation = 0;
 		
-		for (int i = 0; i < fft2.length; i++)
+		for (int i = 34; i < fft2.length/2; i++)
 			correlation += fft2[i]*fft1[i];
 
         return correlation;
--- Sources Info/src/Raphaël/sensitive/Outils.java
+++ Sources Info/src/Raphaël/sensitive/Outils.java
@@ -21,11 +21,47 @@ public class Outils
         int max = -1;
         
         for (int i = 0; i < tab.length; i++)
-            if(tab[i] > max)
+            if(Math.abs(tab[i]) > max)
                 max = tab[i];
                 
         return max;
     }
+    
+    /**
+     * normalise le vecteur suivant la norme 2
+     * et le convertit en double[]
+     * 
+     * @param vect vecteur à normaliser
+     * @return vecteur normalisé
+     */
+    public static double[] normalize(int[] vect)
+    {
+        double som = 0;
+        double[] norm = new double[vect.length];
+        
+        for (int i = 0; i < vect.length; i++)
+            som += vect[i]*vect[i];
+		som = Math.sqrt(som);
+        
+        for (int i = 0; i < vect.length; i++)
+            norm[i] = vect[i]/som;
+        
+        return norm;
+    }
+	
+	public static double[] subMoy(double[] vect)
+	{		
+        double som = 0;
+        
+        for (int i = 0; i < vect.length; i++)
+            som += vect[i];
+		som /= vect.length;
+		
+        for (int i = 0; i < vect.length; i++)
+            vect[i] -= som;
+		
+		return vect;
+	}
     
     public static int[] concatene(int[] debut, int[] fin)
     {
