8241b51e3cd8c92d70b6c42d4471a9a050f5d037
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-12 16:16:13 +0000

    François, faut qu'on parle

---
+++ Sources Web/src/SousProfil.java
@@ -0,0 +1,24 @@
+
+import francois.Salle;
+
+
+/**
+ * un sous-profil est associé à une salle, 
+ * et prend le pas sur le profil global
+ * 
+ * @author raphael
+ */
+public class SousProfil extends ProfilGlobal
+{
+	francois.Salle salle;
+	//TODO :
+	//de cette salle, on récupère l'ensemble de prises
+	//que l'on configure suivant le profil :
+	//on, off ou auto
+
+	public SousProfil(String nom, int temperatureMin, int luminositeGlobale, Salle salle)
+	{
+		super(nom, temperatureMin, luminositeGlobale);
+		this.salle = salle;
+	}
+}
