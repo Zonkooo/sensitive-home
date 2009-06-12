345431bb57d4e8ebc2a8914833e1c4e516971a50
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-12 08:36:00 +0000

    cette classe enregistre la fft d'un bouton et permet de teste la correlation

---
+++ trunk/Sources Info/src/Raphaël/sensitive/Bouton.java
@@ -0,0 +1,25 @@
+package sensitive;
+
+public class Bouton 
+{
+	private double[] fft;
+
+	public Bouton(double[] fft)
+	{
+		this.fft = new double[fft.length];
+		
+		for (int i = 0; i < fft.length; i++)
+			this.fft[i] = fft[i];
+	}
+	
+	public double correlation(double[] fftTap)
+    {
+		double correlation = 0;
+		int len = Math.min(fftTap.length, fft.length);
+		
+		for (int i = 0; i < len; i++)
+			correlation += fft[i]*fftTap[i];
+
+        return correlation;
+    }
+}
