32f36528a9524830c0d286e2233102d08a371ea6
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-15 16:31:56 +0000

    mise en place du debut des profils

--- Sources Web/src/GestionProfils/Prise.java
+++ Sources Web/src/GestionProfils/Prise.java
@@ -2,18 +2,41 @@ package GestionProfils;
 
 public class Prise
 {
-	Etat etat;
-	Appareil app;
+	private Etat etat;
+	private Appareil app;
+	
+	private Multiprise owner;
+	private int position;
 
-	public Prise(Etat etat, Appareil app)
+	public Prise(Etat etat, Appareil app, Multiprise owner, int position)
 	{
 		this.etat = etat;
 		this.app = app;
+		this.owner = owner;
+		this.position = position;
 	}
-	
-	public Prise(Appareil app)
+
+	public Prise(Appareil app, Multiprise owner, int position)
 	{
-		this.etat = Etat.AUTO;
 		this.app = app;
+		this.owner = owner;
+		this.position = position;
+		
+		this.etat = Etat.AUTO;
+	}
+
+	public Multiprise getOwner()
+	{
+		return owner;
+	}
+
+	public int getPosition()
+	{
+		return position;
+	}
+
+	public void setEtat(Etat etat)
+	{
+		this.etat = etat;
 	}
 }
