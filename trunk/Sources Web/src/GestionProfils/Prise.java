ccf82efcce9912afa707d25564b0781c55864e57
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-15 12:32:12 +0000

    changement de package pour rassembler le travail

---
+++ trunk/Sources Web/src/GestionProfils/Prise.java
@@ -0,0 +1,18 @@
+package GestionProfils;
+
+public class Prise {
+	
+	static final boolean ON = true;
+	static final boolean OFF = false;
+	
+	String type;
+	int valeur;
+	boolean etat;
+	
+	public Prise(String t, int val, boolean e){
+		type = t;
+		valeur = val;
+		etat = e;
+	}
+	
+}
---
+++ trunk/Sources Web/src/GestionProfils/ProfilGlobal.java
@@ -0,0 +1,67 @@
+package GestionProfils;
+
+
+/**
+ * Un profil global est appliqué à toute la maison, 
+ * et a une priorité inférieure à celle d'un sous-profil
+ * 
+ * @author raphael
+ */
+public class ProfilGlobal 
+{
+	//TODO : attribuer un ID ?
+	protected String nom;
+	protected int temperature;
+	protected int luminosite;
+
+	public ProfilGlobal(String nom, int temperatureMin, int luminositeGlobale)
+	{
+		this.nom = nom;
+		this.temperature = temperatureMin;
+		this.luminosite = luminositeGlobale;
+	}
+
+	public ProfilGlobal()
+	{
+	}
+
+	//GETTERS
+	public int getLuminosite()
+	{
+		return luminosite;
+	}
+	public String getNom()
+	{
+		return nom;
+	}
+	public int getTemperature()
+	{
+		return temperature;
+	}
+	
+	//SETTERS
+	public void setLuminosite(int luminosite)
+	{
+		this.luminosite = luminosite;
+	}
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+	}
+	public void setTemperature(int temperature)
+	{
+		this.temperature = temperature;
+	}
+	
+	
+
+	@Override public String toString()
+	{
+		return this.nom;
+	}
+	
+	@Override public boolean equals(Object o)
+	{
+		return (this.toString()).equals(o.toString());
+	}
+}
