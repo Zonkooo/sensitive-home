e4cc21bad53deac5cb8e918e45d818bcdcf58236
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-15 12:32:12 +0000

    changement de package pour rassembler le travail

---
+++ Sources Web/src/GestionProfils/Multiprise.java
@@ -0,0 +1,23 @@
+package GestionProfils;
+
+import GestionProfils.Prise;
+
+public class Multiprise {
+	
+	String nom;
+	Prise[] prise;
+	
+	public Multiprise(String n, Prise[] p) {
+		nom =n;
+		prise = p;
+	}
+	
+	public void addPrise(Prise[] p)
+	{
+		prise = p;
+	}
+	
+	public Prise[] getPrises(){
+		return prise;
+	}
+}
---
+++ Sources Web/src/GestionProfils/Salle.java
@@ -0,0 +1,35 @@
+package GestionProfils;
+
+import francois.*;
+import java.util.HashSet;
+
+public class Salle {
+	
+	String nom;
+	Multiprise multiprise;
+	int temperature;
+	int eclairage;
+	
+//	public Salle(String n, Multiprise mp, int t, int e){
+//		nom = n;
+//		multiprise = mp;
+//		temperature = t;
+//		eclairage = e;
+//	}
+	
+	public Salle(String n) {
+		nom = n;
+		multiprise = null;
+		temperature = 0;
+		eclairage = 0;
+	}
+	
+	//n'ajoute pas une mp mais init celle de la salle (une seule mp pour l'instant)
+	public void addMultiprise(Multiprise mp){
+		multiprise = mp;
+		System.out.println("ajout de la mp: "+mp.nom+ " dans la salle: "+this.nom);
+		for(int a=0; a<5; a++){
+			System.out.println("prise"+a+": "+multiprise.getPrises()[a].type);
+		}
+	}
+}
