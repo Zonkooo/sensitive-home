b62a169d33a45bac56956cc56300441c247a336b
cnous3@gmail.com <cnous3@gmail.com@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-16 16:28:32 +0000

    Ajout des attributs ip et communication dans Multiprise AJout de la méthode sendMessage

--- trunk/Sources Web/src/GestionProfils/Multiprise.java
+++ trunk/Sources Web/src/GestionProfils/Multiprise.java
@@ -1,29 +1,38 @@
 package GestionProfils;
 
-public class Multiprise extends Socle
-{
-	public Multiprise(int ID,int taille)
-	{
+import francois.Communication;
+
+public class Multiprise extends Socle {
+	Communication communication;
+	String ip;
+
+	public Multiprise(long ID, int taille , String adresse) {
 		super(ID,taille);
+		ip = adresse;
+		communication = new Communication(ip);
 	}
-	
-	public Multiprise(int ID, Prise[] morceaux)
-	{
+
+	public Multiprise(long ID, Prise[] morceaux, String adresse) {
 		super(ID, morceaux);
+		ip = adresse;
+		communication = new Communication(ip);
 	}
-	
-	public void setPrise(Prise p)
-	{
+
+	public void setPrise(Prise p) {
 		super.setMorceau(p);
 	}
 
-	public Prise getPrise(int index)
-	{
-		return (Prise)(super.getMorceau(index));
+	public Prise getPrise(int index) {
+		return (Prise) (super.getMorceau(index));
 	}
-	
-	public void setEtatPrise(Etat e, int index)
-	{
-		((Prise)(morceaux[index])).setEtat(e);
+
+	public void setEtatPrise(Etat e, int index) {
+		((Prise) (morceaux[index])).setEtat(e);
+	}
+
+	public void sendMessage(String message) {
+		for (int i = 0; i < message.length(); i++) {
+			communication.sendSomeData((byte) message.charAt(i));
+		}
 	}
 }
