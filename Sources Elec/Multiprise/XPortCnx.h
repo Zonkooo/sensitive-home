fb36ba3fc7635c69e415b7584f13fd9e86e1258c
christopher.rabotin <christopher.rabotin@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-17 21:51:12 +0000

    Youpi! La multiprise avec la commande XPort fonctionnne. :)

--- Sources Elec/Multiprise/XPortCnx.h
+++ Sources Elec/Multiprise/XPortCnx.h
@@ -15,20 +15,13 @@ const int recvMsgLength = 7;
 static long time = 0;
 /* hasBeenInit permet de ne pas initialiser deux fois le module XPort
  */ 
-static bool hasBeenInit = false;
+//static bool hasBeenInit = false;
 /* recvDelay est le delais pendant lequel le microcontrolleur va essayer de lire des données séries (en ms)
  */
 static int recvDelay = 1000;
 /* recvIt sert à se positionner dans la chaine reçue depuis le XPort
  */
 static int recvIt = 0;
-
-/* Cette méthode permet d'initialiser la connexion XPort<->PC
- */
-void initXPortCnx();
-/* Permet d'envoyer au PC les données dans le paramètre data
- */
-void sendXPort(const char* data);
 /* Cette méthode est appelée depuis la fonction loop() principale.
  */
 void recvXPort();
