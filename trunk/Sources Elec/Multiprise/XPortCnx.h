546a6fc935dec92636bd4a5bb76b8fcdc635c477
christopher.rabotin <christopher.rabotin@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-18 15:41:56 +0000

    Multiprise sans connexion XBee semble fonctionner de façon optimale pour un module de capteurs.

--- trunk/Sources Elec/Multiprise/XPortCnx.h
+++ trunk/Sources Elec/Multiprise/XPortCnx.h
@@ -18,7 +18,7 @@ static long time = 0;
 //static bool hasBeenInit = false;
 /* recvDelay est le delais pendant lequel le microcontrolleur va essayer de lire des données séries (en ms)
  */
-const int recvDelay = 1000;
+const int recvDelay = 1100;
 /* recvIt sert à se positionner dans la chaine reçue depuis le XPort
  */
 static int recvIt = 0;
