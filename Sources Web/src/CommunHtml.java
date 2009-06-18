fce6a6085ce919685ce2bbea17180a6f1f4726ac
ehau78 <ehau78@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-18 08:29:28 +0000

    configuration profils

--- Sources Web/src/CommunHtml.java
+++ Sources Web/src/CommunHtml.java
@@ -1,21 +1,26 @@
+import gestion_profils.ModuleCapteurs;
+import gestion_profils.Multiprise;
+import gestion_profils.Salle;
+import gestion_profils.ProfilGlobal;
+import gestion_profils.Xml_manipulation;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import francois.Xml_manipulation;
 
 public class CommunHtml {
+	private static String last_salle;
+	private static String last_module;
+	private static String last_multiprise;
+	private static String last_profil;
+	private static boolean force;
+
 	public static void style(PrintWriter out, String userAgent)
 			throws IOException {
 		if (userAgent.contains("iPhone")) {
@@ -34,11 +39,11 @@ public class CommunHtml {
 		out
 				.print("<div class=\"lien_menu\"><a href=\"?page=1\">Accueil</a></div>");
 		out
-				.print("<div class=\"lien_menu\"><a href=\"?page=2\">Configuration capteurs</a></div>");
+				.print("<div class=\"lien_menu\"><a href=\"?page=2\">Configuration Maison</a></div>");
 		out
 				.print("<div class=\"lien_menu\"><a href=\"?page=3\">Changement profil</a></div>");
 		out
-				.print("<div class=\"lien_menu\"><a href=\"?page=4\">Configuration maison</a></div>");
+				.print("<div class=\"lien_menu\"><a href=\"?page=4\">Sauvegarder la configuration</a></div>");
 		out
 				.print("<div class=\"lien_menu\"><a href=\"?page=5\">Configuration profils</a></div>");
 		out.print("</div>");
@@ -58,13 +63,71 @@ public class CommunHtml {
 			out.print("<p>Accueil</p>");
 			break;
 		case 2:
-			Formulaires.capteurs(out,request);
-			break;
-		case 3:
+			// ajout d'une salle
+			if (request.getParameter("nom_salle") != null
+					&& !request.getParameter("nom_salle").equals("")) {
+				creer_salle(request.getParameter("nom_salle"));
+			}
+			// deplacement d'un capteur
+			if (request.getParameter("salle_capteur") != null
+					&& !request.getParameter("salle_capteur").equals(
+							"salle_actuelle")) {
+				bouger_capteur(request);
+			}
+			// deplacement d'une multiprise
+			if (request.getParameter("salle_prise") != null
+					&& !request.getParameter("salle_prise").equals(
+							"salle_actuelle")) {
+				bouger_prise(request);
+			}
+			if (last_salle == null) {
+				last_salle = "";
+			}
+			if (last_module == null) {
+				last_module = "";
+			}
+			if (last_multiprise == null) {
+				last_multiprise = "";
+			}
+			if (force) {
+				force = false;
+				updateCapteurs(request);
+				updateMultiprises(request);
+			}
+			/*
+			if (!last_salle.equals(request.getParameter("salle"))) {
+				force = true;
+				last_salle = request.getParameter("salle");
+			}*/
+			if (last_module.equals(request.getParameter("module"))) {
+				updateCapteurs(request);
+			} else {
+				last_module = request.getParameter("module");
+			}
+
+			if (last_multiprise.equals(request.getParameter("multiprise"))) {
+				updateMultiprises(request);
+			} else {
+				last_multiprise = request.getParameter("multiprise");
+
+			}
+			Formulaires.capteurs(out, request);
 
 			break;
+		case 3:
+			if (last_profil == null) {
+				last_profil = Interface.getHashProfil().values().iterator().next().getNom();
+			}
+			if(last_profil.equals(request.getParameter("profil"))){
+				update_profil(request);
+			}else{
+				last_profil=request.getParameter("profil");
+			}
+			Formulaires.profils(out, request);
+			break;
 		case 4:
-			out.print("<p>Configuration des profils</p>");
+			Xml_manipulation.serialize(Interface.getHashSalle(),"../webapps/web_interface/WEB-INF/classes/francois/config.xml");
+			out.print("<p>Sauvegarde</p>");
 			break;
 		}
 		out.print("</div>");
@@ -99,5 +162,74 @@ public class CommunHtml {
 			out.println("<li>" + str + "</li>");
 
 		out.println("</ul>");
+	}
+
+	public static void updateCapteurs(HttpServletRequest request) {
+		HashMap<String, Salle> hashSalle = Interface.getHashSalle();
+		for (int i = 0; i < 4; i++) {
+			String type = request.getParameter("type" + i);
+			if (type != null) {
+				if (hashSalle.get(request.getParameter("salle")).getModules()
+						.get(Integer.parseInt(request.getParameter("module"))) != null) {
+					hashSalle.get(request.getParameter("salle")).getModules()
+							.get(
+									Integer.parseInt(request
+											.getParameter("module")))
+							.getCapteur(i).setType(
+									Xml_manipulation.getType(type));
+				}
+			}
+		}
+	}
+
+	public static void updateMultiprises(HttpServletRequest request) {
+		HashMap<String, Salle> hashSalle = Interface.getHashSalle();
+		for (int i = 0; i < 5; i++) {
+			String type = request.getParameter("type_mp" + i);
+			if (type != null) {
+				if (hashSalle.get(request.getParameter("salle"))
+						.getMultiprises().get(
+								Integer.parseInt(request
+										.getParameter("multiprise"))) != null) {
+					hashSalle.get(request.getParameter("salle"))
+							.getMultiprises().get(
+									Integer.parseInt(request
+											.getParameter("multiprise")))
+							.getPrise(i)
+							.setType(Xml_manipulation.getType(type));
+				}
+			}
+		}
+	}
+
+	public static void creer_salle(String nom) {
+		HashMap<String, Salle> hashSalle = Interface.getHashSalle();
+		hashSalle.put(nom, new Salle(nom));
+	}
+
+	public static void bouger_capteur(HttpServletRequest request) {
+		HashMap<String, Salle> hashSalle = Interface.getHashSalle();
+		ModuleCapteurs mc = hashSalle.get(request.getParameter("salle"))
+				.getModules().get(
+						Integer.parseInt(request.getParameter("module")));
+		hashSalle.get(request.getParameter("salle_capteur")).addModule(mc);
+
+		hashSalle.get(request.getParameter("salle")).getModules().remove(
+				Integer.parseInt(request.getParameter("module")));
+	}
+	
+	public static void bouger_prise(HttpServletRequest request) {
+		HashMap<String, Salle> hashSalle = Interface.getHashSalle();
+		Multiprise mp = hashSalle.get(request.getParameter("salle")).getMultiprises().get(Integer.parseInt(request.getParameter("multiprise")));
+		hashSalle.get(request.getParameter("salle_prise")).addMultiprise((mp));
+
+		hashSalle.get(request.getParameter("salle")).getMultiprises().remove(
+				Integer.parseInt(request.getParameter("multiprise")));
+	}
+	
+	public static void update_profil(HttpServletRequest request) {
+		HashMap<String, ProfilGlobal> hashProfil = Interface.getHashProfil();
+		hashProfil.get(request.getParameter("profil")).setTemperature(Integer.parseInt(request.getParameter("temperature")));
+		hashProfil.get(request.getParameter("profil")).setLuminosite(Integer.parseInt(request.getParameter("luminosite")));
 	}
 }
