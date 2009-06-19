03ba7c126ab043a374314f13f1ac8269fd141517
cnous3@gmail.com <cnous3@gmail.com@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-19 18:30:01 +0000

    Fred-isation de communication et interfaçage (fait fonctionner en parrallèle la table et la communication avec la multiprise)

--- trunk/Sources Web/src/francois/Lanceur.java
+++ trunk/Sources Web/src/francois/Lanceur.java
@@ -1,12 +1,5 @@
 package francois;
 
-import gestion_profils.Capteur;
-import gestion_profils.Salle;
-import gestion_profils.TypeMorceau;
-import gestion_profils.Xml_manipulation;
-
-import java.io.IOException;
-import java.util.HashMap;
 
 
 
@@ -16,8 +9,24 @@ public class Lanceur {
 
 	
 	public static void main(String[] args){
-		Xml_manipulation.etat_actuel(Xml_manipulation.creation_Hashmap("src/francois/config.xml"),"src/francois/etat.xml");
+//		Xml_manipulation.etat_actuel(Xml_manipulation.creation_Hashmap("src/francois/config.xml"),"src/francois/etat.xml");
 	//	Dhcp.rechercheAdresseXport();
 		
+//		Interface a;
+//		new Interface();  //TODO: créer Interface pour lancer tout le programme
+		
+		Communication communication = new Communication("192.168.0.11");
+		Interfacage interfacage = new Interfacage();
+//		//		
+//		communication.addMessageToQueue("/REQ:0:001\\");
+//		communication.addMessageToQueue("/REQ:2:255\\");
+//		communication.addMessageToQueue("/001144444\\");
+//		interfacage.addBoutonToProfil((new ProfilGlobal("salon",20,20)), true);
+//		
+		communication.start();
+		interfacage.start();
 	}
+	
+	
+	
 }
