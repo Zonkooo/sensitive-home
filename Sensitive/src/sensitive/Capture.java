641021b0dc6d60c6917487c229d064d7cd852e95
vandonr <vandonr@a91cd118-32a7-11de-8a2e-13aa1706fec1>
2009-06-23 20:25:36 +0000

    mega appli de demo pour se toucher

---
+++ Sensitive/src/sensitive/Capture.java
@@ -0,0 +1,220 @@
+package sensitive;
+
+/*
+ * source : http://java.sun.com/products/java-media/sound/samples/JavaSoundDemo/
+ */
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.*;
+
+/**
+ * enregistrement d'un vecteur d'entiers depuis le micro
+ * 
+ * @author raphael
+ */
+public class Capture
+{
+
+	private static final int seuil = 400; //amplitude de declenchement du tap
+	private TargetDataLine line;
+	private AudioFormat format;
+	private int bufferLengthInBytes;
+
+	/**
+	 * Ce constructeur permet de spécifier 
+	 * les paramètres à utiliser pour la capture.
+	 * Par défaut, on est en 44,1 kHz, 16 bits, stereo
+	 * 
+	 * @param sampleRate 44100 - 22050 - 16000 - 11025 - 8000
+	 * @param sampleSizeInBits 16 - 8
+	 * @param channels 2 = stereo - 1 = mono
+	 */
+	public Capture(float sampleRate, int sampleSizeInBits, int channels)
+	{
+		this.format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, sampleRate, sampleSizeInBits, channels, (sampleSizeInBits / 8) * channels, sampleRate, true);
+		init();
+	}
+
+	/**
+	 * Constructeur par defaut :
+	 * 44,1 kHz, 16 bits, stereo
+	 */
+	public Capture()
+	{
+		this(44100, 16, 2);
+	}
+
+	/**
+	 * initialise l'entrée ligne pour une capture 
+	 * à partir des paramètres donnés au constructeur.
+	 * 
+	 * appellée dans le constructeur.
+	 */
+	private void init()
+	{
+		// define the required attributes for our line
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+		//make sure a compatible line is supported.
+		if (!AudioSystem.isLineSupported(info))
+		{
+			System.out.println("Line matching " + info + " not supported.");
+			return;
+		}
+
+		// get the target data line for capture.
+		try
+		{
+			line = (TargetDataLine) AudioSystem.getLine(info);
+		}
+		catch (LineUnavailableException ex)
+		{
+			System.out.println("Unable to open the line: " + ex);
+			return;
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex.toString());
+			return;
+		}
+
+		bufferLengthInBytes = (line.getBufferSize() / 8) * format.getFrameSize();
+	}
+
+	//verrou
+	boolean etalonnage = false;
+	boolean running = false;
+	
+	public void lockEtalon()
+	{
+		this.etalonnage = true;
+	}
+	
+	public void unlockEtalon()
+	{
+		this.etalonnage = false;
+	}
+	
+	public int[][] getTap()
+	{
+		return getTap(false);
+	}
+	
+	/**
+	 * écoute l'entrée ligne jusqu'à capter un signal
+	 * qui dépasse la valeur Capture.seuil.
+	 * 
+	 * @return un echantillon de taille 2*bufferLengthInBytes contenant le tap détecté.
+	 */
+	public int[][] getTap(boolean etalon)
+	{
+		//attente plus ou moins active de la fin de l'étalonnage
+		while(running || etalon != etalonnage)
+		{			
+			try
+			{
+				Thread.sleep(200);
+			}
+			catch (InterruptedException ex)
+			{
+				System.out.println("interruption dans getTap");
+			}
+		}
+		running = true;
+		
+		
+		int[][] audioData = null;
+		byte[] data = new byte[bufferLengthInBytes]; //tampon de lecture
+
+		try
+		{
+			line.open(format, line.getBufferSize());
+		}
+		catch (LineUnavailableException ex)
+		{
+			System.out.println("erreur d'ouverture de la ligne : " + ex.toString());
+		}
+
+		line.start(); //silence, ça tourne
+
+		while (line.read(data, 0, bufferLengthInBytes) != -1)
+		{
+			if (audioData == null && Outils.getMax(traitementBytes(data)) >= Capture.seuil)
+			{
+				audioData = traitementBytes(data);
+			}
+			else if (audioData != null)
+			{
+				audioData = Outils.concatene(audioData, traitementBytes(data));
+				if (audioData[0].length > 20000)
+				{
+					break;
+				}
+			}
+		}
+
+//              for (int i = 0; i < audioData[0].length; i++)
+//                      System.out.println(audioData[0][i] + " " + audioData[1][i]);
+
+		// we reached the end of the stream.  stop and close the line.
+		line.stop();
+		line.close();
+		running = false;
+	
+		return audioData;
+	}
+
+	/**
+	 * Convertit le flux de bytes reçu en un tableau d'int
+	 * contenant les bonnes valeurs des samples
+	 * (si on écoute en 16 bits, il y a 2 bytes par sample
+	 * et il faut les concatener pour avoir la bonne valeur)
+	 * 
+	 * si il y a 2 cannaux, ils sont donnés séparemment 
+	 * dans ret[0][] et ret[1][]
+	 * 
+	 * /!\ l'encodage doit être PCMsigned et BigEndian
+	 * 
+	 * @param audioBytes flux reçu
+	 * @param sampleSize taille en bits d'un sample (8 ou 16)
+	 * @return valeurs des samples
+	 */
+	private int[][] traitementBytes(byte[] audioBytes)
+	{
+		//on suppose qu'on est en PCMsigned, BigEndian
+		int[][] intData = null;
+		int chan = format.getChannels();
+		int nbSamples;
+
+		if (format.getSampleSizeInBits() == 16)
+		{
+			nbSamples = audioBytes.length / 2;
+			intData = new int[chan][nbSamples / chan];
+			int MSB, LSB;
+			for (int i = 0; i < nbSamples; i++)
+			{
+				MSB = (int) audioBytes[2 * i];
+				LSB = (int) audioBytes[2 * i + 1];
+				intData[i % chan][i / chan] = MSB << 8 | (255 & LSB);
+			}
+		}
+		else if (format.getSampleSizeInBits() == 8)
+		{
+			nbSamples = audioBytes.length;
+
+			//si on est en 8 bits, les bytes contiennent déjà les données
+			//il suffit de tout recopier dans un tableau de int
+			//en intercalant 1/2 si on est en stereo
+			intData = new int[chan][nbSamples / chan];
+
+			for (int i = 0; i < nbSamples; i++)
+			{
+				intData[i % chan][i / chan] = audioBytes[i];
+			}
+		}
+
+		return intData;
+	}
+} // End class Capture
+
---
+++ Sensitive/src/sensitive/FFT.java
@@ -0,0 +1,111 @@
+package sensitive;
+
+/*
+ *  source : http://www.ling.upenn.edu/~tklee/Projects/dsp/
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * fft et utilitaires
+ *
+ * @author raphael
+ */
+public class FFT
+{
+    /**
+     * Complete le signal sig avec des zeros
+     * jusqu'à atteindre une longeur qui soit une puissance de 2
+     *
+     * @param sig signal originel
+     * @return signal de longueur une puissance entière de 2
+     */
+    public static int[] zeroPadding(int[] sig)
+    {
+        int[] padded = new int[1 << ((Outils.lg(sig.length)) + 1)];
+        for(int i = 0; i < sig.length; i++)
+            padded[i] = sig[i];
+
+        return padded;
+    }
+
+    //TODO : comprendre & commenter
+    private static int bitrev(int j1, int nu)
+    {
+        int j2;
+        int k = 0;
+        for (int i = 1; i <= nu; i++)
+        {
+            j2 = j1 / 2;
+            k = 2 * k + j1 - 2 * j2;
+            j1 = j2;
+        }
+        return k;
+    }
+
+    //TODO : comprendre & commenter
+    public static double[] fftMag(int[] x)
+    {
+        // assume n is a power of 2
+        int n = x.length;
+        int nu = Outils.lg(n);
+        int n2 = n / 2;
+        int nu1 = nu - 1;
+        double[] xre = new double[n];
+        double[] xim = new double[n];
+        double[] mag = new double[n2];
+        double tr, ti, p, arg, c, s;
+
+        for (int i = 0; i < n; i++)
+            xre[i] = (double)x[i];
+
+                int k = 0;
+        for (int l = 1; l <= nu; l++)
+        {
+            while (k < n)
+            {
+                for (int i = 1; i <= n2; i++)
+                {
+                    p = bitrev(k >> nu1, nu);
+                    arg = 2 * (double) Math.PI * p / n;
+                    c = (double) Math.cos(arg);
+                    s = (double) Math.sin(arg);
+                    tr = xre[k + n2] * c + xim[k + n2] * s;
+                    ti = xim[k + n2] * c - xre[k + n2] * s;
+                    xre[k + n2] = xre[k] - tr;
+                    xim[k + n2] = xim[k] - ti;
+                    xre[k] += tr;
+                    xim[k] += ti;
+                    k++;
+                }
+                k += n2;
+            }
+            k = 0;
+            nu1--;
+            n2 = n2 / 2;
+        }
+        k = 0;
+        int r;
+        while (k < n)
+        {
+            r = bitrev(k, nu);
+            if (r > k)
+            {
+                tr = xre[k];
+                ti = xim[k];
+                xre[k] = xre[r];
+                xim[k] = xim[r];
+                xre[r] = tr;
+                xim[r] = ti;
+            }
+            k++;
+        }
+
+        for (int i = 0; i < n/2; i++)
+        {
+            mag[i] = Math.sqrt(xre[i] * xre[i] + xim[i] * xim[i]);
+        }
+        return mag;
+    }
+}
